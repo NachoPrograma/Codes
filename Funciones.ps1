fbf1163d206c4458bcbca111b85c3101e05cc967
PROFESOR <cfgs.ddi@gmail.com>
2021-12-21 15:18:20 +0100

    Nuevas funciones

--- Funciones.ps1
+++ Funciones.ps1
@@ -18,5 +18,11 @@ function saludo2 ($nombre)
 {
     Write-Host "Hola" $nombre  # Se ejecuta cuando se llama a la funcion saludo2
 }
-
 saludo2("Rafa") # Llamada a la funcion saludo2 con el argumento de entrada "Rafa"
+
+
+#Funcion con 2 argumentos de entrada
+function saludo3 ($nombre, $edad){
+    Write-Host El usuario $nombre tiene la edad de $edad
+}
+saludo3("Carlos",23)
